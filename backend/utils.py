381e4af59381d75e5320c2f7197221f53de4de64
JavierRodriguezFontiveros <javierrodriguezfontiveros@gmail.com>
2024-12-07 13:47:00 +0100

    pusheo

--- backend/utils.py
+++ backend/utils.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 def connect_to_db():
     # Cargar las variables de entorno desde el archivo .env
-    load_dotenv()
+    load_dotenv(dotenv_path="../credendiciales.env")
     
     # Configura los parámetros de la conexión
     db_host = os.getenv("DB_HOST_AWS")
@@ -13,6 +13,13 @@ def connect_to_db():
     db_password = os.getenv("DB_PASSWORD_AWS")
     db_database = os.getenv("DB_DATABASE_AWS")
     db_port = int(os.getenv("DB_PORT_AWS", 5432))
+
+
+    print(f"DB Host: {db_host}")
+    print(f"DB User: {db_username}")
+    print(f"DB Password: {db_password}")
+    print(f"DB Database: {db_database}")
+    print(f"DB Port: {db_port}")
 
 
     print
@@ -36,3 +43,5 @@ def connect_to_db():
     except Exception as error:
         print("Error desconocido:", error)
         return None  
+    
+connect_to_db()
